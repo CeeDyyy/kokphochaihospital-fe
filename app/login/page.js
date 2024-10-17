db12c112f6e69a0bf0464d7afb9d760f15445142
lugaYY <oraya.yyh@gmail.com>
2024-10-17 21:15:07 +0700

    Updated Login Page

--- app/login/page.js
+++ app/login/page.js
@@ -1,6 +1,5 @@
 'use client'
-import { Input } from "@material-tailwind/react";
-import { Button } from "@material-tailwind/react";
+import { Input, Button } from "@material-tailwind/react";
 
 export default function Login() {
     return (
@@ -8,14 +7,15 @@ export default function Login() {
             <div className="h-full flex justify-center items-center">
                 <div>
                     <div className="w-72 space-y-4">
+                        <p className="text-6xl mb-12">Login</p>
                         <Input label="Username" />
                         <Input label="Password" />
-                        <div class="flex flex-row-reverse space-x-4 space-x-reverse">
-                            <Button color="blue">Sign-in</Button>
+                        <div className="flex justify-end">
+                            <Button color="blue" className="w-56">Sign-in</Button>
                         </div>
-                        <div class="flex flex-row-reverse space-x-1 space-x-reverse">
-                            <a href="#" class="no-underline hover:underline text-xs">set new password</a>
-                            <p class="text-xs">Forget Password?</p>
+                        <div class="flex justify-end space-x-1">
+                            <p className="text-xs">Forget Password?</p>
+                            <a href="#" className="no-underline hover:underline text-xs text-[#1E88E5]">set new password</a>
                         </div>
                     </div>
                 </div>
