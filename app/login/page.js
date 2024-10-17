d6eb4ea26d1e4c70e10ee3d106376839423cc3d5
lugaYY <oraya.yyh@gmail.com>
2024-10-17 21:33:36 +0700

    use state username and password

--- app/login/page.js
+++ app/login/page.js
@@ -1,17 +1,35 @@
 'use client'
+
+import React, { useState } from "react";
 import { Input, Button } from "@material-tailwind/react";
 
 export default function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleLogin() {
+        if (username) {
+            console.log(username);
+        } else {
+            console.error("Please enter the username")
+        }
+        if (password) {
+            console.log(password);
+        } else{
+            console.error("Please enter password")
+        }
+    }
+
     return (
         <div className="h-screen md:px-96">
             <div className="h-full flex justify-center items-center">
                 <div>
                     <div className="w-72 space-y-4">
                         <p className="text-6xl mb-12">Login</p>
-                        <Input label="Username" />
-                        <Input label="Password" />
+                        <Input label="Username" onChange={(e) => setUsername(e.target.value)} />
+                        <Input label="Password" type="password" onChange={(e) => setPassword(e.target.value)} />
                         <div className="flex justify-end">
-                            <Button color="blue" className="w-56">Sign-in</Button>
+                            <Button color="blue" className="w-56" onClick={() => handleLogin()}>Sign-in</Button>
                         </div>
                         <div class="flex justify-end space-x-1">
                             <p className="text-xs">Forget Password?</p>
