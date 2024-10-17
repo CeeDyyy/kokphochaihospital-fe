a8aa1399905a9590786a0d6e3b6febef56e70499
lugaYY <oraya.yyh@gmail.com>
2024-10-17 21:52:33 +0700

    Created Contact Page

---
+++ app/contact/page.js
@@ -0,0 +1,21 @@
+
+export default function Contact() {
+
+
+    return (
+        <div className="h-screen md:px-96">
+            <div className="h-full flex flex-col items-center pt-32">
+                <p className="text-6xl"> Contact </p>
+
+                <div className="grid grid-cols-2 w-full">
+                    <div className="border">
+                        <p>TEST1</p>
+                    </div>
+                    <div className="border">
+                        <p>TEST2</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
