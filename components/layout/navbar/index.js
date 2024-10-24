0d52214874f1fe66046535b01d70bdd9aec2f512
CeeDyyy <cdnoob@gmail.com>
2024-10-24 17:57:43 +0700

    drawer update

--- components/layout/navbar/index.js
+++ components/layout/navbar/index.js
@@ -61,36 +61,34 @@ export default function Navbar() {
         </button>
       </nav>
       <Drawer
-        placement="right"
         open={openRight}
+        placement="right"
+        overlay={false}
         onClose={() => setOpenRight(false)}
-        className="p-4"
+        className="grid content-between p-4"
       >
         <div className="grid gap-y-8">
           <div className="flex justify-between">
-            <button onClick={() => router.push('/')} className="flex pointer">
+            <button onClick={() => router.push('/')} className="flex items-center gap-x-2 pointer">
               <img src="/images/KPC_LOGO.png" width={48} height={48} />
+              <p>{language.hospital}</p>
             </button>
             <button onClick={() => setOpenRight(false)}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={2}
-                stroke="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="h-5 w-5"><path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
             </button>
           </div>
           {language.menu.map((page, index) =>
             <button onClick={() => { router.push(page.path); setOpenRight(false); }} className={`py-2 px-4 ${pathname === page.path ? "text-blue-500" : ""} hover:bg-gray-100 active:bg-gray-300 rounded-lg duration-150`} key={index}>{page.title}</button>
           )}
+        </div>
+        <div className="flex justify-between">
+          <button onClick={() => router.push('/login')} className="p-2 hover:bg-gray-100 rounded-full duration-150">
+            <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth={0} /><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" /><g id="SVGRepo_iconCarrier"> <path d="M5 21C5 17.134 8.13401 14 12 14C15.866 14 19 17.134 19 21M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="#000000" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" /> </g></svg>
+          </button>
+          <select onChange={(e) => router.push(pathname + '?' + createQueryString('lang', e.target.value))}>
+            <option value="th">TH</option>
+            <option value="en">EN</option>
+          </select>
         </div>
       </Drawer>
     </>
