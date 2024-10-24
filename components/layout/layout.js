230d67e591f5aeaa8321fcc9f3016336b23c46e1
CeeDyyy <cdnoob@gmail.com>
2024-10-24 16:28:53 +0700

    layout update

---
+++ components/layout/layout.js
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react';
+
+export default function Layout({ children, title }) {
+
+    return (
+        <div className="min-h-[72vh] py-32 px-8 md:px-96 grid gap-20">
+            <p className="text-center text-3xl md:text-6xl">{title}</p>
+            {children}
+        </div>
+    );
+}
