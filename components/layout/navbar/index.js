d5b9daf95f7e91a50ed0297c3a14b05895eb2bac
CeeDyyy <cdnoob@gmail.com>
2024-10-24 22:24:14 +0700

    className update

--- components/layout/navbar/index.js
+++ components/layout/navbar/index.js
@@ -47,11 +47,11 @@ export default function Navbar() {
             <button onClick={() => router.push(page.path)} className={`py-2 px-4 ${pathname === page.path ? "text-blue-500" : ""} hover:bg-gray-100 active:bg-gray-300 rounded-lg duration-150`} key={index}>{page.title}</button>
           )}
         </div>
-        <div className="hidden lg:flex">
+        <div className="hidden lg:flex gap-x-2 items-center">
           <button onClick={() => router.push('/login')} className="p-2 hover:bg-gray-100 rounded-full duration-150">
             <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth={0} /><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" /><g id="SVGRepo_iconCarrier"> <path d="M5 21C5 17.134 8.13401 14 12 14C15.866 14 19 17.134 19 21M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="#000000" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" /> </g></svg>
           </button>
-          <select onChange={(e) => router.push(pathname + '?' + createQueryString('lang', e.target.value))}>
+          <select className="h-fit py-1 px-2 hover:bg-gray-100 rounded-lg duration-150 cursor-pointer" onChange={(e) => router.push(pathname + '?' + createQueryString('lang', e.target.value))}>
             <option value="th">TH</option>
             <option value="en">EN</option>
           </select>
@@ -81,11 +81,11 @@ export default function Navbar() {
             <button onClick={() => { router.push(page.path); setOpenRight(false); }} className={`py-2 px-4 ${pathname === page.path ? "text-blue-500" : ""} hover:bg-gray-100 active:bg-gray-300 rounded-lg duration-150`} key={index}>{page.title}</button>
           )}
         </div>
-        <div className="flex justify-between">
+        <div className="flex justify-between items-center">
           <button onClick={() => router.push('/login')} className="p-2 hover:bg-gray-100 rounded-full duration-150">
             <svg width="32px" height="32px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth={0} /><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" /><g id="SVGRepo_iconCarrier"> <path d="M5 21C5 17.134 8.13401 14 12 14C15.866 14 19 17.134 19 21M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z" stroke="#000000" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" /> </g></svg>
           </button>
-          <select onChange={(e) => router.push(pathname + '?' + createQueryString('lang', e.target.value))}>
+          <select className="h-fit py-1 px-2 hover:bg-gray-100 rounded-lg duration-150 cursor-pointer" onChange={(e) => router.push(pathname + '?' + createQueryString('lang', e.target.value))}>
             <option value="th">TH</option>
             <option value="en">EN</option>
           </select>
